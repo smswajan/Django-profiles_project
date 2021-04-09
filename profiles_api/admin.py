3125157127777ee7aa049b647d5003eb646cc0a8
SHAHJAHAN MD. SWAJAN <swajan.mis13.du@gmail.com>
2021-04-09 17:47:35 +0600

    added user profile feed API

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import UserProfile
+from .models import ProfileFeedItem
 
 # Register your models here.
 
 admin.site.register(UserProfile)
+admin.site.register(ProfileFeedItem)
